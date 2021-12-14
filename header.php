e203df1172f8125034461ded411169fc0ea8f2ef
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-12-14 12:51:57 +0100

    6b

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php bloginfo( 'name' ); ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <?php wp_head() ?>
 </head>
 
@@ -13,3 +18,4 @@
     <h1><?php bloginfo( 'name' ); ?></h1>
     <h4><?php bloginfo( 'description' ); ?></h4>
 </header>
+<div class="container">
